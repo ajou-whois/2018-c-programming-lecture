c98d60cde7da64eac26f61bce8a32e49e2a3792e
JunGu Kang <chr0m3@users.noreply.github.com>
2018-04-30 23:58:09 +0900

    Fix typo in 1_24.c

--- example_code/day1/1_24.c
+++ example_code/day1/1_24.c
@@ -3,8 +3,8 @@
 int main(void) {
     int a = 10;
 
-    printf("size of a: %ld", sizeof a);
-    printf("size of int: %ld", sizeof (int));  // 자료형에는 괄호를 쓴다
+    printf("size of a: %ld\n", sizeof a);
+    printf("size of int: %ld\n", sizeof (int));  // 자료형에는 괄호를 쓴다
 
     return 0;
 }
