b4fe6ac3394ba51bdc740713f5cbc8d24bb18afd
Chr0m3 <chr0m3.kr@gmail.com>
2018-05-07 19:30:11 +0900

    Change Format String on 2_24.c

--- example_code/day2/2_24.c
+++ example_code/day2/2_24.c
@@ -3,8 +3,8 @@
 int main(void) {
     int matrix[10][10];
 
-    printf("matrix size: %ld\n", sizeof matrix);
-    printf("matrix length: %ld\n", sizeof matrix / sizeof (int));
+    printf("matrix size: %u\n", sizeof matrix);
+    printf("matrix length: %u\n", sizeof matrix / sizeof (int));
 
     return 0;
 }
